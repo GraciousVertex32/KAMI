c5e7176d88ca7bced9e49f28c1516241b814ee0f
GraciousVertex32 <31588270+GraciousVertex32@users.noreply.github.com>
2017-12-08 20:55:42 +0800

    12.8

---
+++ KAMI/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace KAMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadImage = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(267, 157);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoadImage
+            // 
+            this.LoadImage.Location = new System.Drawing.Point(714, 112);
+            this.LoadImage.Name = "LoadImage";
+            this.LoadImage.Size = new System.Drawing.Size(97, 39);
+            this.LoadImage.TabIndex = 1;
+            this.LoadImage.Text = "Load Image";
+            this.LoadImage.UseVisualStyleBackColor = true;
+            this.LoadImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(714, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Try";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(586, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "16";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(586, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(510, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Height";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1006, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(267, 146);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 182);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1289, 568);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(913, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 762);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LoadImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button LoadImage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ KAMI/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KAMI
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img;//loaded image
+        Bitmap convert;//converted image
+        Grid[,] imggrid = new Grid[16, 10];
+        Color[,] Color=new Color[16,10];//unclassifed color map for loaded image
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//load image
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                img = new Bitmap(ofd.FileName);
+                pictureBox1.Image = img;
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)//image working
+        {
+            int Width = int.Parse(textBox1.Text);//16
+            int Height = int.Parse(textBox2.Text);//10
+            int pixelength = img.Width / Width;
+            int hpl = pixelength / 2;
+            convert = new Bitmap(Width, Height);
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Color[x,y] = GridReader(x * pixelength + hpl, y * pixelength + hpl);
+                    convert.SetPixel(x, y, Color[x,y]);
+                    textBox3.Text+= Classify(Color[x, y]);//change parameter
+                }
+                textBox3.Text += "\r\n";
+                textBox3.Text += "\r\n";
+            }
+            pictureBox2.Image = convert;
+            label3.Text = Color[15, 0].GetBrightness().ToString();//test brightness
+        }
+        public string Classify(Color c)//classify color tybe
+        {
+            float hue = c.GetHue();
+            float lgt = c.GetBrightness();
+            float sat = c.GetSaturation();
+            double satthreshold = 0.35;
+            if (lgt < 0.2 ) return "黑 " ;
+            if (hue < 20 && sat > satthreshold) return "红 " ;
+            if (hue < 90 && sat > satthreshold) return "黄 " ;
+            if (hue < 150 && sat > satthreshold) return "绿 " ;
+            if (hue < 210 && sat > satthreshold) return "青 ";
+            if (hue < 270 && sat > satthreshold) return "蓝 " ;
+            if (hue < 330 && sat > satthreshold) return "紫 " ;
+            if (hue < 360 && sat > satthreshold) return "红 ";
+            return "黑 " ;
+        }
+        public string Classify(Color c,int a)//for configuration
+        {
+            float hue = c.GetHue();
+            float lgt = c.GetBrightness();
+            float sat = c.GetSaturation();
+            double satthreshold=0.35;
+            if (lgt < 0.2) return "黑 " + lgt + sat;
+            if (hue < 20 && sat > satthreshold) return "红 " + lgt + sat;
+            if (hue < 90 && sat > satthreshold) return "黄 " + lgt + sat;
+            if (hue < 150 && sat > satthreshold) return "绿 " + lgt + sat;
+            if (hue < 210 && sat > satthreshold) return "青 " + lgt + sat;
+            if (hue < 270 && sat > satthreshold) return "蓝 " + lgt + sat;
+            if (hue < 330 && sat > satthreshold) return "紫 " + lgt + sat;
+            if (hue < 360 && sat > satthreshold) return "红 " + lgt + sat;
+            return "黑 " + lgt + sat;
+        }
+        public Color GridReader(int x, int y)//get average color for a grid
+        {
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            int radius = 5;
+            for (int a = x - radius; a <= x + radius; a++)
+            {
+                for (int b = y - radius; b <= y + radius; b++)
+                {
+                    red += img.GetPixel(a, b).R;
+                    green += img.GetPixel(a, b).G;
+                    blue += img.GetPixel(a, b).B;
+                }
+            }
+            red = (red / 121);
+            green = (green / 121);
+            blue = (blue / 121);
+            Color gridcolor = System.Drawing.Color.FromArgb(red, green, blue);
+            return gridcolor;
+        }
+
+
+
+        public void Init()//set up the grid array
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 16; x++)
+                {
+                    Grid grid = new Grid();
+                    grid.x = x;
+                    grid.y = y;
+                    grid.gridcolor = Color[x, y];
+                    imggrid[x, y] = grid;
+                }
+            }
+        }
+        public class Grid//single color grid
+        {
+            public Color gridcolor;
+            public Area area;
+            public int x;
+            public int y;
+            public Boolean IsSameColor(Grid neighbour)//check if neighbour grid is same color
+            {
+                return gridcolor.Equals(neighbour);
+            }
+            public void Connect(Grid neighbour)//connect same color neighbour
+            {
+                if (neighbour.area == null && this.area == null) 
+                {
+                    Area area = new Area();
+                    area.list.Add(this);
+                    area.list.Add(neighbour);
+                }
+                if (this.area == null && neighbour.area != null)
+                {
+                    this.area = neighbour.area;
+                    neighbour.area.list.Add(this);
+                }
+                if (this.area != null && neighbour.area == null)
+                {
+                    neighbour.area = this.area;
+                    this.area.list.Add(neighbour);
+                }
+                if (this.area != null && neighbour.area != null && !(this.area.list.SequenceEqual(neighbour.area.list)))//connect after color change
+                {
+                    this.area.list.AddRange(neighbour.area.list);
+                    neighbour.area.list = this.area.list;
+                    neighbour.area = this.area;
+                }
+            }
+            public void ChangeColor(Color newcolor)//change area color
+            {
+                foreach (Grid grid in this.area.list)
+                {
+                    grid.gridcolor = newcolor;
+                }
+            }
+        }
+        public void CheckConnection(Grid current)//check surround grids and connect them if match color
+        {
+            if (current.x > 0)
+            {
+                if (current.IsSameColor(imggrid[current.x - 1, current.y]))
+                {
+                    current.Connect(imggrid[current.x - 1, current.y]);
+                }
+            }
+            if (current.x < 16)
+            {
+                if (current.IsSameColor(imggrid[current.x + 1, current.y]))
+                {
+                    current.Connect(imggrid[current.x + 1, current.y]);
+                }
+            }
+            if (current.y > 0)
+            {
+                if (current.IsSameColor(imggrid[current.x, current.y - 1]))
+                {
+                    current.Connect(imggrid[current.x, current.y - 1]);
+                }
+            }
+            if (current.y < 16)
+            {
+                if (current.IsSameColor(imggrid[current.x, current.y + 1]))
+                {
+                    current.Connect(imggrid[current.x, current.y + 1]);
+                }
+            }
+        }
+        public class Area
+        {
+            public List<Grid> list;
+            public void AreaConnection()//do this after color change to merge areas
+            {
+                Form1 a = new Form1();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    a.CheckConnection(list[i]);
+                }
+            }
+        }
+        
+    }
+}
